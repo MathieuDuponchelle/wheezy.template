d2872177fba519a873b8720e30aa36f0d1970d4b
Mathieu Duponchelle <mathieu@centricular.com>
2023-02-23 10:48:27 +0100

    comp: don't increment lineno
    See https://github.com/akornatskyy/wheezy.template/issues/68

--- src/wheezy/template/comp.py
+++ src/wheezy/template/comp.py
@@ -5,5 +5,5 @@ from _thread import allocate_lock  # noqa
 
 def adjust_source_lineno(source: str, name: str, lineno: int) -> typing.Any:
     node = compile(source, name, "exec", ast.PyCF_ONLY_AST)
-    ast.increment_lineno(node, lineno)
+    # ast.increment_lineno(node, lineno)
     return node
